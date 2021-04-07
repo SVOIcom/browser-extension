7fef91a6acdffe2d6e206da2670e9394965a63fd
Andrey Nedobylsky <andrey@izzz.io>
2021-04-07 19:58:29 +0300

    Free ton instancer

---
+++ modules/const/WalletContract.mjs
@@ -0,0 +1,51 @@
+import Utils from "../utils.mjs";
+
+class WalletContract {
+
+    static WALLET_TYPES = {
+        SafeMultisig: 'SafeMultisig',
+        SurfMultisig: 'SurfMultisig'
+    }
+
+    /**
+     * Returns SafeMultisig wallet
+     * @returns {Promise<{imageBase64: *, abi: *}>}
+     * @constructor
+     */
+    static async  SafeMultisig() {
+        return {
+            abi: await Utils.fetchJSON('/abi/SafeMultisigWallet.abi.json'),
+            imageBase64: await Utils.fetchJSON('/abi/contracts/SafeMultisigWallet.base64.json')
+        }
+    }
+
+    static async  SurfMultisig() {
+        return {
+            abi: await Utils.fetchJSON('/abi/SurfMultisigWallet.abi.json'),
+            imageBase64: await Utils.fetchJSON('/abi/contracts/SurfMultisigWallet.base64.json')
+        }
+    }
+
+    /**
+     * Get wallet
+     * @param type
+     * @returns {Promise<{imageBase64: *, abi: *}>}
+     */
+    static async  getWalletData(type = WalletContract.WALLET_TYPES.SafeMultisig) {
+        switch (type) {
+            case 'SafeMultisig':
+            case 'SafeMultisigWallet':
+            case 'SafeMultisigVerified':
+            default:
+                return WalletContract.SafeMultisig();
+
+            case 'SurfMultisig':
+            case 'Surf':
+            case 'SURF':
+            case 'surf':
+                return WalletContract.SurfMultisig();
+        }
+    }
+}
+
+export default WalletContract;
--- modules/freeton/FreetonCrypto.mjs
+++ modules/freeton/FreetonCrypto.mjs
@@ -6,6 +6,8 @@
    |_| \___/|_| \_|  \_/\_/ \__,_|_|_|\___|\__|
 
  */
+import FreetonInstance from "./FreetonInstance.mjs";
+
 /**
  * @name FreeTON browser wallet and injector
  * @copyright SVOI.dev Labs - https://svoi.dev
@@ -13,9 +15,41 @@
  * @version 1.0
  */
 
+export const MNEMONIC_DICTIONARY = {
+    TON: 0,
+    ENGLISH: 1,
+    CHINESE_SIMPLIFIED: 2,
+    CHINESE_TRADITIONAL: 3,
+    FRENCH: 4,
+    ITALIAN: 5,
+    JAPANESE: 6,
+    KOREAN: 7,
+    SPANISH: 8,
+};
 
-class FreetonCrypto{
+export const SEED_LENGTH = {
+    w12: 12,
+    w24: 24
+}
 
+export const HD_PATH = "m/44'/396'/0'/0/0";
+
+class FreetonCrypto {
+
+    static async generateSeed(dict = MNEMONIC_DICTIONARY.ENGLISH, length = SEED_LENGTH.w12) {
+        const ton = await FreetonInstance.getFreeTON();
+        return await ton.crypto.mnemonicFromRandom({dictionary: dict, wordCount: length});
+    }
+
+    static async seedToKeypair(seed, dict = MNEMONIC_DICTIONARY.ENGLISH, length = SEED_LENGTH.w12) {
+        const ton = await FreetonInstance.getFreeTON();
+        return await ton.crypto.mnemonicDeriveSignKeys({
+            dictionary: dict,
+            wordCount: length,
+            phrase: seed,
+            path: HD_PATH
+        });
+    }
 
 }
 
