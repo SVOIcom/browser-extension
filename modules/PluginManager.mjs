d73ee669b990086a58c83dbdfa641e7cc233d1b7
Andrey Nedobylsky <admin@twister-vl.ru>
2022-05-19 11:24:07 +0200

    Load module pages

--- modules/PluginManager.mjs
+++ modules/PluginManager.mjs
@@ -1,11 +1,13 @@
 import utils from "./utils.mjs";
 import uiUtils from "./ui/uiUtils.mjs";
 
-const PLUGINS_JSON = 'https://svoicom.github.io/scalewallet-plugins/plugins.json';
+
+const PLUGINS_BASE = 'https://plugins.scalewallet.com';
 
 class PluginManager {
-    async init(pluginsJson = PLUGINS_JSON) {
-        this.plugins = (await utils.fetchJSON(pluginsJson)).plugins;
+    async init(pluginsBase = PLUGINS_BASE) {
+        this.pluginsBase = pluginsBase;
+        this.plugins = (await utils.fetchJSON(pluginsBase + '/plugins.json')).plugins;
 
         this._pluginsRuntime = {};
 
@@ -28,20 +30,60 @@ class PluginManager {
                                             </div>
                                         </a>
                                     </li>`;
-                pluginRuntime.pluginMenuButton = $('#pluginMenuList').append(appendStr);
+                $('#pluginMenuList').append(appendStr);
+                pluginRuntime.pluginMenuButton = $(`#${plugin.path}_menuButton`);
                 pluginRuntime.menuButtonAction = plugin.menuAction;
 
-                $(pluginRuntime.pluginMenuButton).on('click',async ()=>{
-                    if(pluginRuntime.menuButtonAction.page){
+                $(`#${plugin.path}_menuButton`).on('click', async () => {
+                    if(pluginRuntime.menuButtonAction.page) {
 
                         await uiUtils.navigateUrlAsync("/pluginPage");
+
+                        let contentIframe = document.getElementById('moduleContentIframe');
+                        pluginRuntime.contentIframe = contentIframe;
+
+                        //Resolve page path
+                        let contentSrc = pluginRuntime.menuButtonAction.page.content;
+                        if(!contentSrc.includes('https://')) {
+                            contentSrc = `${this.pluginsBase}/plugins/${plugin.path}/${contentSrc}`;
+                        }
+
+                        console.log('LOAD', contentSrc, plugin);
+
+                        //Set iframe path
+                        contentIframe.src = contentSrc;
+
+                        //Set plugin page title
                         $('#pluginTitle').text(pluginRuntime.menuButtonAction.page.title);
+
+                        //iframe.contentWindow.postMessage({a:123}, '*')
+
+
+                        //Allow emulation in frame
+                        await this.evalPluginContentIframe(plugin.path, `_everscaleWalletConfig = {EVERWalletEmulation: true};`)
                     }
                 })
             }
 
             this._pluginsRuntime[plugin.path] = pluginRuntime;
         }
+    }
+
+
+    async sendPluginContentIframeMessage(modulePath, message) {
+        if(this._pluginsRuntime[modulePath]) {
+            if(this._pluginsRuntime[modulePath].contentIframe) {
+                this._pluginsRuntime[modulePath].contentIframe.contentWindow.postMessage(message, '*')
+            } else {
+                throw new Error('Content iframe not initialized');
+            }
+        } else {
+            throw new Error('Plugin not found');
+        }
+    }
+
+    async evalPluginContentIframe(modulePath, code) {
+        await this.sendPluginContentIframeMessage(modulePath, {type: 'eval', code})
     }
 }
 
