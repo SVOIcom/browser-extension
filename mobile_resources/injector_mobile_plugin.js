7382442c316db7aee6b6f3844593fa5f412d748d
Andrey Nedobylsky <admin@twister-vl.ru>
2022-08-05 14:41:52 +0200

    Test

--- mobile_resources/injector_mobile_plugin.js
+++ mobile_resources/injector_mobile_plugin.js
@@ -21,17 +21,22 @@ console.log('HELLO INJECTOR PLUGIN MOBILE');
             return;
         }
 
+        if(event.data.stopCycling){
+            console.log('STOP CYCLING');
+            return;
+        }
+
         //Outcome RPC to page
         if(event.data.method) {
 
-            window.parent.postMessage({'!':true, ...event.data, sender: 'page'}, "*");
+            window.parent.postMessage({stopCycling:true, ...event.data, sender: 'page'}, "*");
             try{
-                cordova_iab.postMessage(JSON.stringify({'!':true, ...event.data, sender: 'page', senderMore: {url: window.location}}))
+                cordova_iab.postMessage(JSON.stringify({stopCycling:true, ...event.data, sender: 'page', senderMore: {url: window.location}}))
             }catch (e) {
             }
 
             try{
-                window.webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({'!':true, ...event.data, sender: 'page', senderMore: {url: window.location}}))
+                window.webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({stopCycling:true, ...event.data, sender: 'page', senderMore: {url: window.location}}))
             }catch (e) {
             }
             //cordova_iab.postMessage(JSON.stringify({...event.data, sender: 'page', senderMore: {url: window.location}}))
